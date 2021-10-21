dc925a97fa5a1fd329084db4898e427d4db41a08
Kushal <st.kushal@gmail.com>
2021-10-21 12:31:26 +0530

    Feat: Progress Bars that animate one after the other

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+/* animationQueue: Array of progress bar ids. All progress bars will be animated sequentially from this queue */
 
 var progressBars = 0;
+var animationQueue = [];
 function addProgressBar() {
-    /* Disable button till the progress bar is 100% complete */
-    document.getElementById('addProgressBar').disabled = true;
-
     progressBars++;
 
     /* Creating a new div element */
@@ -24,28 +23,53 @@ function addProgressBar() {
     /* Injecting the newly created div as a child */
     parentProgressBar[0].appendChild(newProgressBar);
 
-    /* Setting the width of progress bar as 1 */
-    let width = 1;
+    /* If animationQueue is empty, add new progress bar to queue and animate */
+    if (animationQueue.length < 1) {
+        animationQueue.push(id)
+        animate();
+    }
+    /* If animationQueue is not empty and there is an animating progress bar, push to animationQueue */
+    else {
+        animationQueue.push(id)
+    }
+}
 
-    /* To go from 0px to 200px in 5 seconds 
-       200px -> 200 steps; 
-       5 seconds -> 5000 milliseconds
-       To calculate how many milliseconds should lapse before a step is incremented, 5000/200 = 25 => 25 milliconds should lapse to increase 1px */
+function animate() {
+    if (animationQueue.length > 0) {
+        /* Setting the width of progress bar as 1 */
+        let width = 1;
 
-    /* Call scene function after every 25 millisecond to increment 1 step*/
-    let identity = setInterval(scene, 25);
-    function scene() {
-        if (width >= 200) {
-            /* Once the progress bar is complete, clear the interval */
-            clearInterval(identity);
+        /* To go from 0px to 200px in 5 seconds 
+           200px -> 200 steps; 
+           5 seconds -> 5000 milliseconds
+           To calculate how many milliseconds should lapse before a step is incremented, 5000/200 = 25 => 25 milliconds should lapse to increase 1px */
 
-            /* Enable back the button to add more progress bars */
-            document.getElementById('addProgressBar').disabled = false;
-        } else {
+        /* Call scene function after every 25 millisecond to increment 1 step*/
+        let identity = setInterval(scene, 25);
+        function scene() {
+            if (width >= 200) {
+                /* Once the progress bar is complete, clear the interval */
+                clearInterval(identity);
 
-            /* Increment to add 1px to progress bar */
-            width++;
-            document.getElementById(id).style.width = width + "px";
+                /* Remove the progressBar id at 0th position of animmationQueue after completion */
+                dequeue();
+
+                /* If there are pending ids in the animationQueue, animate them */
+                if (animationQueue.length > 0) {
+                    animate();
+                }
+            } else {
+                width++;
+
+                /* Add width to the the progressBar at 0th position of animmationQueue */
+                document.getElementById(animationQueue[0]).style.width = width + "px";
+            }
         }
     }
+}
+
+function dequeue() {
+    if (animationQueue.length > 0)
+        /* Remove the first element (0th position) from animationQueue */
+        animationQueue.shift()
 }
