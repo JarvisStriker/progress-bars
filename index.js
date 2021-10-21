b29f29effa7687513028ca3995831994b9561b86
Kushal <st.kushal@gmail.com>
2021-10-21 13:56:23 +0530

    Feat: Control buttons with toggle speed for progress bars

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* barWidth: Object that contains widths of all progress bars -> {progressBarID : width} */
 /* barStatus: Object that contains start/stop status of all progress bars -> {progressBarId : status} */
+/* barSpeed: Object that contains ms/step values of all progress bars -> {progressBarId : barSpeed} */
 
 var progressBars = 0;
 var barWidth = {};
 var barStatus = {};
+var barSpeed = {};
 
 function addProgressBar() {
     progressBars++;
@@ -23,6 +25,7 @@ function addProgressBar() {
     startButton.onclick = function (event) {
         let barId = event.path[0].id.replace('start_', '')
         barStatus[barId] = true
+        barSpeed[barId] = 25
         animate(barId)
     };
 
@@ -51,6 +54,32 @@ function addProgressBar() {
         setTimeout(() => { barStatus[barId] = true; animate(barId) }, 30)
     };
 
+    /* Creating a new button with text 'Toggle Speed' */
+    const toggleSpeedButton = document.createElement("button");
+    toggleSpeedButton.innerHTML = "Toggle Speed";
+    toggleSpeedButton.style.margin = "0em 1em"
+    toggleSpeedButton.setAttribute('id', 'speed_' + id)
+
+    /* Change speed between 15 ms/step and 25 ms/step  */
+
+    toggleSpeedButton.onclick = function (event) {
+        let barId = event.path[0].id.replace('speed_', '')
+        barStatus[barId] = false
+
+        /* To go from 0px to 200px in 3 seconds 
+           200px -> 200 steps; 
+           3 seconds -> 3000 milliseconds
+           To calculate how many milliseconds should lapse before a step is incremented, 3000/200 = 25 => 15 milliconds should lapse to increase 1px */
+
+        /* To go from 0px to 200px in 5 seconds 
+           200px -> 200 steps; 
+           5 seconds -> 5000 milliseconds
+           To calculate how many milliseconds should lapse before a step is incremented, 5000/200 = 25 => 25 milliconds should lapse to increase 1px */
+
+        barSpeed[barId] = barSpeed[barId] == 15 ? 25 : 15;
+        setTimeout(() => { barStatus[barId] = true; animate(barId) }, 30)
+    };
+
     newProgressBar.classList.add("progressBar");
     newProgressBar.setAttribute('id', id)
 
@@ -61,19 +90,14 @@ function addProgressBar() {
     parentProgressBar[0].appendChild(startButton)
     parentProgressBar[0].appendChild(stopButton)
     parentProgressBar[0].appendChild(resetButton)
+    parentProgressBar[0].appendChild(toggleSpeedButton)
 
 }
 
 function animate(id) {
     if (!barWidth[id])
         barWidth[id] = 1;
-    /* To go from 0px to 200px in 5 seconds 
-       200px -> 200 steps; 
-       5 seconds -> 5000 milliseconds
-       To calculate how many milliseconds should lapse before a step is incremented, 5000/200 = 25 => 25 milliconds should lapse to increase 1px */
-
-    /* Call scene function after every 25 millisecond to increment 1 step*/
-    let identity = setInterval(scene, 25);
+    let identity = setInterval(scene, barSpeed[id]);
     function scene() {
         if (barWidth[id] >= 200 || !barStatus[id]) {
             /* Once the progress bar is complete or if the status is false(stop), clear the interval */
