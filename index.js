bf4d4d670d1bb7edadfcc2b60f4e5e9469b32451
Kushal <st.kushal@gmail.com>
2021-10-21 13:02:32 +0530

    Feat: Maximum of 3 Progress Bars that animate at any given time

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-/* animationQueue: Array of progress bar ids. All progress bars will be animated sequentially from this queue */
+/* animationQueue: Array of progress bar ids that are not in priorityQueue.*/
+/* priorityQueue: Array of progress bar ids that are animating. Length is always less than or equal to 3.
+   If length decreases from 3, first element of animationQueue is popped & pushed into priorityQueue */
 
 var progressBars = 0;
 var animationQueue = [];
+var priorityQueue = [];
+
 function addProgressBar() {
     progressBars++;
 
@@ -23,22 +27,26 @@ function addProgressBar() {
     /* Injecting the newly created div as a child */
     parentProgressBar[0].appendChild(newProgressBar);
 
-    /* If animationQueue is empty, add new progress bar to queue and animate */
-    if (animationQueue.length < 1) {
-        animationQueue.push(id)
-        animate();
-    }
-    /* If animationQueue is not empty and there is an animating progress bar, push to animationQueue */
-    else {
-        animationQueue.push(id)
+    /* If priortQueue has space to accomodate another progress bar, we push the new progress bar id to priorityQueue else we push it to animationQueue*/
+    if (priorityQueue.length < 3) {
+        priorityQueue.push(id);
+        switch (priorityQueue.length) {
+            /* Based on the priority, assign progress bar to different animate functions */
+            case 1: animate1(id);
+                break;
+            case 2: animate2(id);
+                break;
+            case 3: animate3(id);
+                break;
+        }
+    } else {
+        animationQueue.push(id);
     }
 }
 
-function animate() {
-    if (animationQueue.length > 0) {
-        /* Setting the width of progress bar as 1 */
-        let width = 1;
-
+function animate1(id) {
+    if (priorityQueue.length > 0) {
+        let width1 = 1;
         /* To go from 0px to 200px in 5 seconds 
            200px -> 200 steps; 
            5 seconds -> 5000 milliseconds
@@ -46,30 +54,103 @@ function animate() {
 
         /* Call scene function after every 25 millisecond to increment 1 step*/
         let identity = setInterval(scene, 25);
+
         function scene() {
-            if (width >= 200) {
+            if (width1 >= 200) {
                 /* Once the progress bar is complete, clear the interval */
                 clearInterval(identity);
 
-                /* Remove the progressBar id at 0th position of animmationQueue after completion */
-                dequeue();
+                /* Remove the refernce from local lexical environment */
+                width1 = null;
 
-                /* If there are pending ids in the animationQueue, animate them */
-                if (animationQueue.length > 0) {
-                    animate();
+                /* Remove the progress bar id from priorityQueue after animating */
+                priorityQueue.splice(priorityQueue.indexOf(id), 1)
+                if (animationQueue.length > 0 && priorityQueue.length < 3) {
+                    /* Pop & Push next element from animationQueue to priorityQueue */
+                    let nextElement = animationQueue.shift();
+                    priorityQueue.push(nextElement)
+                    animate1(nextElement)
                 }
             } else {
-                width++;
+                width1++;
 
-                /* Add width to the the progressBar at 0th position of animmationQueue */
-                document.getElementById(animationQueue[0]).style.width = width + "px";
+                /* Add width to the progressBar with id received as parameter */
+                document.getElementById(id).style.width = width1 + "px";
             }
         }
     }
 }
 
-function dequeue() {
-    if (animationQueue.length > 0)
-        /* Remove the first element (0th position) from animationQueue */
-        animationQueue.shift()
+function animate2(id) {
+
+    if (priorityQueue.length > 0) {
+        let width2 = 1;
+        /* To go from 0px to 200px in 5 seconds 
+          200px -> 200 steps; 
+          5 seconds -> 5000 milliseconds
+          To calculate how many milliseconds should lapse before a step is incremented, 5000/200 = 25 => 25 milliconds should lapse to increase 1px */
+
+        /* Call scene function after every 25 millisecond to increment 1 step*/
+        let identity = setInterval(scene, 25);
+        function scene() {
+            if (width2 >= 200) {
+                /* Once the progress bar is complete, clear the interval */
+                clearInterval(identity);
+
+                /* Remove the refernce from local lexical environment */
+                width2 = null;
+
+                /* Remove the progress bar id from priorityQueue after animating */
+                priorityQueue.splice(priorityQueue.indexOf(id), 1)
+                if (animationQueue.length > 0 && priorityQueue.length < 3) {
+                    /* Pop & Push next element from animationQueue to priorityQueue */
+                    let nextElement = animationQueue.shift();
+                    priorityQueue.push(nextElement)
+                    animate2(nextElement)
+                }
+            } else {
+                width2++;
+                /* Add width to the progressBar with id received as parameter */
+
+                document.getElementById(id).style.width = width2 + "px";
+            }
+        }
+    }
+}
+
+function animate3(id) {
+
+    if (priorityQueue.length > 0) {
+        let width3 = 1;
+        /* To go from 0px to 200px in 5 seconds 
+          200px -> 200 steps; 
+          5 seconds -> 5000 milliseconds
+          To calculate how many milliseconds should lapse before a step is incremented, 5000/200 = 25 => 25 milliconds should lapse to increase 1px */
+
+        /* Call scene function after every 25 millisecond to increment 1 step*/
+        let identity = setInterval(scene, 25);
+        function scene() {
+            if (width3 >= 200) {
+                /* Once the progress bar is complete, clear the interval */
+                clearInterval(identity);
+
+                /* Remove the refernce from local lexical environment */
+                width3 = null;
+
+                /* Remove the progress bar id from priorityQueue after animating */
+                priorityQueue.splice(priorityQueue.indexOf(id), 1)
+                if (animationQueue.length > 0 && priorityQueue.length < 3) {
+                    /* Pop & Push next element from animationQueue to priorityQueue */
+                    let nextElement = animationQueue.shift();
+                    priorityQueue.push(nextElement)
+                    animate3(nextElement)
+                }
+            } else {
+                width3++;
+
+                /* Add width to the progressBar with id received as parameter */
+                document.getElementById(id).style.width = width3 + "px";
+            }
+        }
+    }
 }
